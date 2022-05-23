23924b06c914f1a4c41310abe8ed6c16d793448d
loulima <louizalima13@gmail.com>
2022-05-23 12:38:52 -0300

    components register + login

--- my-app/src/Pages/Register.js
+++ my-app/src/Pages/Register.js
@@ -1,4 +1,6 @@
 import logotipo from '../img/logotipo.svg';
+import {BtnSubmit} from "../Components/ButtonSubmit";
+import {Input} from "../Components/Input";
 
 function Register() {
     return(
@@ -9,18 +11,60 @@ function Register() {
         
         <form className="register-form">
             <h2>Criar Conta </h2>
-                
-                <label className="label-text">Nome</label>
-                <label className="label-text">Email</label>
-                <label className="label-text">Senha</label>
+                <section>
+                    <Input
+                    type="text"
+                    label="Nome"
+                    // value={name}
+                    placeholder="Digite o seu nome completo"
+                    required = "required"
+                    />
+                    <br />
+                    <Input
+                    type="email"
+                    label="E-mail"
+                    // value={email}
+                    name="input"
+                    placeholder="user@user.com"
+                    required = "required"
+                    />
+                    <br />
+                    <Input
+                    type="password"
+                    label="Senha"
+                    // value={password}
+                    name="input"
+                    placeholder="******"
+                    required = "required"
+                    />
+                    <br />
+                </section>
 
                 <section className="role-register">
                     <h3> Escolha seu cargo:</h3>
-                    <label>Atendente</label>
-                    <label>Cozinha</label>
+                    <br />
+                    <Input
+                    type="radio"
+                    className="input-radio"
+                    label="Atendente"
+                    // value="saloon"
+                    // name="role"
+                    // checked={role === "saloon"}
+                    // onChange={(e) => setRole(e.target.value)}
+                    />
+                    <br />
+                    <Input
+                    type="radio"
+                    className="input-radio"
+                    label="Cozinha"
+                    // value="kitchen"
+                    // name="role"
+                    // checked={role === "kitchen"}
+                    // onChange={(e) => setRole(e.target.value)}
+                    />
+                    <br />
                 </section>
-                {/* <Button type="submit" btnText="Cadastrar" btnOnclick={handleSubmit} /> */}
-                <button type="submit" className="btnSubmitRegister"> Registrar </button>
+                <BtnSubmit text="Cadastrar"/>
         </form></>
     );
     };
